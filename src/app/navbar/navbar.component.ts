e43db5ce265f5acdc9adbdaa69571d527c5c3187
Valter Furtado Seeberger <valtercesa@gmail.com>
2021-08-06 18:47:59 -0300

    Commit inicial

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'Navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
